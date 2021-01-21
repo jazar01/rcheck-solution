72af76a7eeb430128b12673b0288c30621f35ec6
jazar01 <jim.azar@orasi.com>
2021-01-21 10:41:10 -0500

    Bump version number

--- rcheck/Properties/AssemblyInfo.cs
+++ rcheck/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("rcheck")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("RCheck")]
+[assembly: AssemblyDescription("RCheck CLI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("rcheck")]
+[assembly: AssemblyCompany("Orasi Software, Inc")]
+[assembly: AssemblyProduct("RCheck")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.0.1")]
+[assembly: AssemblyFileVersion("1.1.0.1")]
